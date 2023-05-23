4604a6308fd56d44b61ba53432aa90b9343038d8
J Pranav <pranavofficial12@gmail.com>
2023-05-23 11:58:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HarshRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Register from './Components/Register';
 import Login from './Components/Login';
